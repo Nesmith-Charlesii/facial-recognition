9c714beba377e1543d5dd831807631c6db18b793
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-10-15 19:28:43 -0400

    added commentation for better understanding

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import numpy as np
 import cv2 as cv 
 
+# Load the pre-trained model for face detection - this is a Haar Cascade classifier provided by OpenCV
 face_classifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 def detect_faces(img):
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    # Convert color space of img to grayscale (required for face detection)
+    gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray_img, 1.3, 5)
+    
     if faces is ():
+        print("No face detected")
         return img
     
     for (x,y,w,h) in faces:
         cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
     return img
 
-cap = cv.VideoCapture(0)
+# cv.VideoCapture sets up the webcam for video capture - 0 is the default camera
+cam_capture = cv.VideoCapture(0)
 
 tracking = True
 while tracking:
-    ret, frame = cap.read()
+    ret, frame = cam_capture.read()
     frame = detect_faces(frame)
     cv.imshow('Video Face Detection', frame)
-    
+
     keypress = cv.waitKey(1)
     if keypress == ord('q'):
         tracking = False
 
-cap.release()
+cam_capture.release()
 cv.destroyAllWindows()
